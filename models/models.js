272727d8bd4057461f4a62b9076e40c4f2b3a3ed
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-08-21 17:58:50 -0500

    Acaptación del modelo para el despliegue en Heroku.  - Modificamos el archivo 'models.js' y agregamos la implementación para el uso de 'PostgreSQL' en Heroku y 'SQLite' en el entorno de desarrollo local.  - Se elimina el archivo de la BD 'quiz.sqlite'

--- models/models.js
+++ models/models.js
@@ -1,17 +1,33 @@
 var path = require( 'path' );
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 //-> Carga Modelo ORM (Sequelize)
 var Sequelize = require( 'sequelize' );
 
-//-> Usa BD SQlite
+//-> Usa BD SQlite o PostgreSQL
 var sequelize = new Sequelize(
-	null,
-	null,
-	null,
-	{
-		dialect: 'sqlite',
-		storage: 'quiz.sqlite'	
-	}
+	DB_name, 
+	user, 
+	pwd, 
+    { 
+	  	dialect:  protocol,
+	    protocol: protocol,
+	    port:     port,
+	    host:     host,
+	    storage:  storage,  // solo SQLite (.env)
+	    omitNull: true      // solo PostgreSQL
+    }      
 );
 
 //-> Importa la definicion de la tabla 'Quiz' en 'quiz.js'
